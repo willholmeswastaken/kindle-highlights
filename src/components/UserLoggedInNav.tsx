a021ace33602f5af115e0e407934c4c5e5766e81
Will Holmes <willholmes97@icloud.com>
2022-07-12 21:31:09 +0100

    Tidied up the view of the dashboard with nav

--- src/components/UserLoggedInNav.tsx
+++ src/components/UserLoggedInNav.tsx
@@ -1,42 +1,64 @@
-import React from 'react'
-import { HoverLink } from '.'
-import IdentityPanel from './identity/identityPanel'
-import { CollectionIcon, SwitchHorizontalIcon, TemplateIcon } from '@heroicons/react/solid';
+import React from "react";
+import { HoverLink } from ".";
+import IdentityPanel from "./identity/identityPanel";
+import {
+    CollectionIcon,
+    SwitchHorizontalIcon,
+    TemplateIcon,
+} from "@heroicons/react/solid";
+import { NextRouter, useRouter } from "next/router";
+
+interface NavItem {
+    icon: JSX.Element;
+    linkUrl: string;
+    displayName: string;
+}
+
+const navItems: NavItem[] = [
+    {
+        icon: <TemplateIcon className="inline-block h-6 w-6 m-2" />,
+        linkUrl: "/dashboard",
+        displayName: "Dashboard",
+    },
+    {
+        icon: <CollectionIcon className="inline-block h-6 w-6 m-2" />,
+        linkUrl: "/import-history",
+        displayName: "Import History",
+    },
+    {
+        icon: <SwitchHorizontalIcon className="inline-block h-6 w-6 m-2" />,
+        linkUrl: "/import",
+        displayName: "Import",
+    },
+];
 
 const UserLoggedInNav = () => {
+    const router: NextRouter = useRouter();
+    const isCurrentRoute: (route: string) => boolean = (route) =>
+        router.pathname.toLowerCase() === route.toLowerCase();
     return (
         <>
             <div className="hidden sm:flex sm:flex-col mt-4 py-8 px-5">
                 <div className="mb-8">
                     <IdentityPanel />
                 </div>
-                <HoverLink active>
-                    <div className="flex flex-row w-fit p-1">
-                        <TemplateIcon className="inline-block h-6 w-6 m-2" />
-                        <div className="flex flex-col">
-                            <span className="pl-2 mt-2">Dashboard</span>
+                {navItems.map((x) => (
+                    <HoverLink
+                        key={x.displayName}
+                        active={isCurrentRoute(x.linkUrl)}
+                        linkUrl={x.linkUrl}
+                    >
+                        <div className="flex flex-row w-fit p-1">
+                            {x.icon}
+                            <div className="flex flex-col">
+                                <span className="pl-2 mt-2">{x.displayName}</span>
+                            </div>
                         </div>
-                    </div>
-                </HoverLink>
-                <HoverLink>
-                    <div className="flex flex-row w-fit p-1">
-                        <CollectionIcon className="inline-block h-6 w-6 m-2" />
-                        <div className="flex flex-col">
-                            <span className="pl-2 mt-2">Import History</span>
-                        </div>
-                    </div>
-                </HoverLink>
-                <HoverLink>
-                    <div className="flex flex-row w-fit p-1">
-                        <SwitchHorizontalIcon className="inline-block h-6 w-6 m-2" />
-                        <div className="flex flex-col">
-                            <span className="pl-2 mt-2">Import</span>
-                        </div>
-                    </div>
-                </HoverLink>
+                    </HoverLink>
+                ))}
             </div>
         </>
-    )
-}
+    );
+};
 
-export default UserLoggedInNav
+export default UserLoggedInNav;
--- src/components/identity/identityPanel.tsx
+++ src/components/identity/identityPanel.tsx
@@ -1,35 +1,29 @@
 import { useSession, signIn, signOut } from "next-auth/react"
+import { NextRouter, useRouter } from 'next/router'
 import { HoverLink } from '../';
 import IdentityDetails from "./identityDetails";
 
 const IdentityPanel = (): JSX.Element => {
-    const { data: session } = useSession()
-    console.log(session);
+  const { data: session } = useSession()
+  const router: NextRouter = useRouter()
 
-    const onLogout = () => {
-        const shouldLogout = confirm('Are you sure that you want to logout?');
-        if (!shouldLogout) return;
-        signOut();
-    }
+  const onLinkClick = () => {
+    if (session)
+      router.push('/account');
+    else
+      signIn();
+  };
 
-    const onLogin = () => signIn();
-    const onLinkClick = () => {
-        if (session)
-            onLogout();
-        else
-            onLogin();
-    };
-
-    return (
-        <>
-            <HoverLink onClick={onLinkClick}>
-                <IdentityDetails
-                    name={session?.user?.name ?? 'Sign In'}
-                    image={session?.user?.image ?? 'https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png'}
-                />
-            </HoverLink>
-        </>
-    )
+  return (
+    <>
+      <HoverLink onClick={onLinkClick}>
+        <IdentityDetails
+          name={session?.user?.name ?? 'Sign In'}
+          image={session?.user?.image ?? 'https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png'}
+        />
+      </HoverLink>
+    </>
+  )
 }
 
 export default IdentityPanel;
